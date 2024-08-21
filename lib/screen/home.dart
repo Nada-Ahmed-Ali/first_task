702756b852ff3d33d6fdb521262213996589ecda
Nada-Ahmed-Ali <sciencec064@gmail.com>
2024-08-21 07:25:35 +0300

    Initial commit

---
+++ lib/screen/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NameScreen extends StatelessWidget {
+  const NameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Nada',
+        style: TextStyle(
+          fontSize: 50,color: Colors.blue,
+          backgroundColor: Colors.black
+            
+
+
+          ),
+        ),
+      ),
+
+
+    );
+  }
+}
